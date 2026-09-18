f43eec3a692dacf2f0a963467b9823168d288fde
Hajar1426 <hajar1426@gmail.com>
2026-09-18 23:02:55 +0300

    Add contact form interaction with JavaScript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("JavaScript is connected!");
+const contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    alert("Thank you for your message!");
+
+    contactForm.reset();
+});
